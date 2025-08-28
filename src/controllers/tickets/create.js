104ffb604e3f73fd4090a676b30e29b001967cb3
Walleska Maier <walleskamaier@gmail.com>
2025-08-27 23:29:27 -0300

    feat: ✨ implement ticket creation

--- src/controllers/tickets/create.js
+++ src/controllers/tickets/create.js
@@ -1,3 +1,17 @@
+import { randomUUID } from "node:crypto"
+
 export function create({ request, response }) {
-  return response.end("Ticket created")
+  const { equipment, description, user_name } = request.body
+
+  const ticket = {
+    id: randomUUID(),
+    equipment,
+    description,
+    user_name,
+    status: "open",
+    created_at: new Date(),
+    updated_at: new Date(),
+  }
+
+  return response.end(JSON.stringify(ticket))
 }
