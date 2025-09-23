4dcf1d3200ccfa489cdbb444c2c0744548d1b18a
Walleska Maier <walleskamaier@gmail.com>
2025-09-23 15:20:03 -0300

    feat: ✨ implement JSON database layer

--- src/database/database.js
+++ src/database/database.js
@@ -29,8 +29,17 @@ export class Database {
     this.#persist()
   }
 
-  select(table) {
+  select(table, filters) {
     let data = this.#database[table] ?? []
+
+    if (filters) {
+      data = data.filter((row) => {
+        return Object.entries(filters).some(([key, value]) => {
+          return row[key].toLowerCase().includes(value.toLowerCase()) === value
+        })
+      })
+    }
+
     return data
   }
 }
