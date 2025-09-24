f3fe4dd0da7ee0391b7ccb8f3d062d683d7cf264
Walleska Maier <walleskamaier@gmail.com>
2025-09-23 23:13:01 -0300

    feat: ✨ add update method, controller and PUT /tickets/:id endpoint

---
+++ src/controllers/tickets/update.js
@@ -0,0 +1,8 @@
+export function update({ request, response, database }) {
+  const { id } = request.params
+  const { equipment, description } = request.body
+
+  database.update("tickets", id, { equipment, description, updated_at: new Date() })
+
+  return response.end()
+}
